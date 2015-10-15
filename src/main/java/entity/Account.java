0c3d604f70ba57b664d3e64738c7745a906569c4
SenioR <iseniuori@gmail.com>
2015-10-16 00:28:09 +0300

    Add account message logic

--- src/main/java/entity/Account.java
+++ src/main/java/entity/Account.java
@@ -1,7 +1,62 @@
 package entity;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  *
  */
+@Entity
+@Table(name = "account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name="access_token")
+    private String accessToken;
+    @Column(name = "data")
+    private Date date;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Account() {
+    }
 }
